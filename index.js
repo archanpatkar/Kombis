f7c2f4cd48f393f55b171290d907cc80bc10e645
archanpatkar <pat@archanpatkar.com>
2018-08-22 19:35:02 +0530

    Complete

--- index.js
+++ index.js
@@ -44,15 +44,21 @@ function ast(parsed)
         char = parsed[0];
         parsed.shift();
     }
+    if(char == ")")
+    {
+        parsed.push(")");
+    }
     return node;
 }
 
 let I = (x) => x;
 let K = (x) => (y) => x;
+let S = (x) => (y) => (z) => (x(z))(y(z));
 
 let combinators = {
-    "I":I,
-    "K":K
+    "I":{ c:I , params:1 },
+    "K":{ c:K , params:2 },
+    "S":{ c:S , params:3 }
 }
 
 function hasArray(node)
@@ -66,20 +72,38 @@ function hasArray(node)
 
 function transform(tree)
 {
-    console.log(tree);
-    for(let node of tree)
-    {
-        console.log(node);
-        if(hasArray(node))
+    tree = Array.from(tree);
+    let transformed = [];
+    for(let n in tree)
+    {    
+        let node = tree[n];
+        let c = node[0];
+        node.shift();
+        if(Array.isArray(c)) transformed.push(transform([c]));
+        if(c in combinators)
         {
-            transform(node);
-        }
-        else
-        {
-
-        }
+            let comb = combinators[c];
+            let values = [];
+            for(let v = 0; v < comb.params; v++)
+            {
+                const val = node[v];
+                if(Array.isArray(val)) values.push(transform([val]));
+                else if(val in combinators) values.push(`${combinators[val].c}`);
+                else if(val != undefined) values.push(val);
+            }
+            if(values.length > 0)
+            {
+                let params = "";
+                for(let v of values) params += `(${v})`
+                transformed.push(`(${comb.c})${params}`);
+            }
+            else
+            {
+                transformed.push(`(${comb.c})`);
+            }
+        }   
     }
-    return tree;
+    return transformed;
 }
 
 function combi(string,...values)
@@ -87,8 +111,50 @@ function combi(string,...values)
     let parsed = partial(string);
     parsed = fill(parsed,values);
     let tree = ast(parsed);
-    return tree;
+    let transformed = transform(tree);
+    return transformed
+            .reduce((acc, val) => acc.concat(val), [])
+            .map(tfc => eval(tfc))
+            .map( eval => {
+                if(eval instanceof Function) return eval.toString()
+                else return eval;
+            });
 }
 
-let test1 = combi`( K ( I ( I #${true} ) ) ) ( I #${false} ) )`;
-console.log(JSON.stringify(test1));
+// Random
+console.log("RANDOM");
+let test1 = combi`((K(I#${false})(I(I#${10}))))`;
+console.log(test1);
+
+let test2 = combi`(SII#${(x)=>10})`;
+console.log(test2);
+
+// Truth Values
+console.log("TRUTH VALUES");
+const TRUE = combi`(K)`;
+console.log(TRUE);
+
+const FALSE = combi`(SK)`;
+console.log(FALSE);
+
+// Not
+console.log("NOT");
+const not_of_false = combi`((SK(SK)(K)))`;
+console.log(not_of_false);
+
+const not_of_true = combi`((K(SK)(K)))`;
+console.log(not_of_true);
+
+// OR
+console.log("OR");
+const or_t_t = combi`(K(K)(K))`
+console.log(or_t_t);
+
+const or_t_f = combi`(K(K)(SK))`
+console.log(or_t_f);
+
+const or_f_t = combi`(SK(SK)(K))`
+console.log(or_f_t);
+
+const or_f_f = combi`(SK(SK)(SK))`
+console.log(or_f_f);
