8ca7b442e219e7400214d1d9eb47d68efe0a9519
archanpatkar <pat@archanpatkar.com>
2018-10-16 12:14:18 +0530

    Update

--- Backyard.js
+++ Backyard.js
@@ -1,36 +1,37 @@
-function partial(string,values)
-{
-    string = string.map(element => element.split(""));
-    let partial = "";
-    for(let s in string)
-    {
-        if(Array.isArray(s))
-        {
-            for(let i of s) partial += i;
-        }
-        else
-        {
-            partial += s;
-        }
+// function partial(string,values)
+// {
+//     string = string.map(element => element.split(""));
+//     let partial = "";
+//     for(let s in string)
+//     {
+//         if(Array.isArray(s))
+//         {
+//             for(let i of s) partial += i;
+//         }
+//         else
+//         {
+//             partial += s;
+//         }
 
-    }
-    return partial;
-}
+//     }
+//     return partial;
+// }
 
-function fill(partial,values)
-{
-    let complete = [];
-    let value = "";
-    let counter = 0;
-    for(let i in partial)
-    {
-        value = partial[i];
-        if(value == " ");
-        else if(value == "#") complete.push(values[counter++]);
-        else complete.push(value);
+// function fill(partial,values)
+// {
+//     let complete = [];
+//     let value = "";
+//     let counter = 0;
+//     for(let i in partial)
+//     {
+//         value = partial[i];
+//         if(value == " ");
+//         else if(value == "#") complete.push(values[counter++]);
+//         else complete.push(value);
         
-    }
-    return complete;
-}
-let parsed = partial(string);
-parsed = fill(parsed,values);
+//     }
+//     return complete;
+// }
+// let parsed = partial(string);
+// parsed = fill(parsed,values);
+
