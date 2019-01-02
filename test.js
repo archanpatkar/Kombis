0ae36e24325c4141c26244dd7a5dca9c9f392592
archanpatkar <pat@archanpatkar.com>
2019-01-02 12:15:34 +0530

    Update

--- test.js
+++ test.js
@@ -1,25 +1,25 @@
 const { combi , combc } = require("./index");
 
-// // Random
-// console.log("RANDOM");
-// let test1 = combi`((K(I ${false})(I(I ${10}))))`;
-// console.log(test1);
+// Random
+console.log("RANDOM");
+let test1 = combi`((K(I ${false})(I(I ${10}))))`;
+console.log(test1);
 
 
-// let test2 = combi`(SII ${(x)=>10})`;
-// console.log(test2);
+let test2 = combi`(SII ${(x)=>10})`;
+console.log(test2);
 
-// // Testing
-// console.log("Testing")
-// let mp = combc`(SKISKI)`;
-// console.log(mp[0].toString());
+// Testing
+console.log("Testing")
+let mp = combc`(SKISKI)`;
+console.log(mp[0].toString());
 
-// // Self Application a.k.a. Mockingbird
-// console.log("SELF APPLICATION");
-// let sa = combi`(SII ${(x) => 10})`;
-// console.log(sa[0].toString());
+// Self Application a.k.a. Mockingbird
+console.log("SELF APPLICATION");
+let sa = combi`(SII ${(x) => 10})`;
+console.log(sa[0].toString());
 
-// console.log("IRREDUCIBLE PROPERTY");
+console.log("IRREDUCIBLE PROPERTY");
 // SII(SII) = I(SII)(I(SII)) = I(SII)(SII) = SII(SII) ...[Infinite Recursion 💣]
 // Uncomment the lines below to see the effect
 // let limitsa = combi`(SII(SII))`;
@@ -37,39 +37,44 @@ const { combi , combc } = require("./index");
 // let limitsa = combi`(Y ${x => log(10)})`;
 // console.log(limitsa[0]);
 
-// console.log("REVERSAL");
-// let limitsa = combi`(S(K(SI))K ${x=>20} ${y=>10})`;
-// console.log(limitsa[0]);
+console.log("REVERSAL");
+let limitsa = combi`(S(K(SI))K ${x=>20} ${y=>10})`;
+console.log(limitsa[0]);
 
 
-// // Truth Values
-// console.log("TRUTH VALUES");
-// const TRUE = combi`(K)`;
-// console.log(TRUE[0].toString());
+// Truth Values
+console.log("TRUTH VALUES");
+const TRUE = combi`(K)`;
+console.log(TRUE[0].toString());
 
-// const FALSE = combi`(SK)`;
-// console.log(FALSE[0].toString());
+const FALSE = combi`(SK)`;
+console.log(FALSE[0].toString());
 
-// // Not
-// console.log("NOT");
-// const not_of_false = combi`((SK(SK)(K)))`;
-// console.log(not_of_false[0].toString());
+// Not
+console.log("NOT");
+const not_of_false = combi`((SK(SK)(K)))`;
+console.log(not_of_false[0].toString());
 
-// const not_of_true = combi`((K(SK)(K)))`;
-// console.log(not_of_true[0].toString());
+const not_of_true = combi`((K(SK)(K)))`;
+console.log(not_of_true[0].toString());
 
-// // OR
-// console.log("OR");
-// const or_t_t = combi`(K(K)(K))`
-// console.log(or_t_t[0].toString());
+// OR
+console.log("OR");
+const or_t_t = combi`(K(K)(K))`
+console.log(or_t_t[0].toString());
 
-// const or_t_f = combi`(K(K)(SK))`
-// console.log(or_t_f[0].toString());
+const or_t_f = combi`(K(K)(SK))`
+console.log(or_t_f[0].toString());
 
-// const or_f_t = combi`(SK(SK)(K))`
-// console.log(or_f_t[0].toString());
+const or_f_t = combi`(SK(SK)(K))`
+console.log(or_f_t[0].toString());
 
-// const or_f_f = combi`(SK(SK)(SK))`
-// console.log(or_f_f[0].toString());
+const or_f_f = combi`(SK(SK)(SK))`
+console.log(or_f_f[0].toString());
 
 
+console.log(combc`(
+    (*Jag* ${(x)=>(y)=>(z)=>z}) 
+    (IJagIKI)
+    (KI)
+)`);
